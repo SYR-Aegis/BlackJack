c28c21ccdebb845de261813e1d824fc3895a5c57
dangdang1ing15 <shine_0512@naver.com>
2020-08-21 14:14:34 +0900

    finish

--- scv.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
-
-pic = open('img/cards-[C0]-001.xml', 'r')
-soup = BeautifulSoup(pic,'html.parser')
-
-for annotation in soup.findAll('size'):
-    print(annotation['width'], annotation['height'])
-    print(annotation.title.string, annotation.length.string)
